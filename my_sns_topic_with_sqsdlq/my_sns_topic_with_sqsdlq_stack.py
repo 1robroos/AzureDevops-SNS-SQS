d743220d6916897da644a2b44356098bb46b4ebd
Rob Roos <rob.roos@athlon.com>
2024-05-11 19:48:23 +0200

    optimize validation

--- my_sns_topic_with_sqsdlq/my_sns_topic_with_sqsdlq_stack.py
+++ my_sns_topic_with_sqsdlq/my_sns_topic_with_sqsdlq_stack.py
@@ -9,21 +9,13 @@ class MySnsTopicWithSqsdlqStack(Stack):
   def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
-    # Resources
-    # eventsEventBus00default00hob1r = events.CfnEventBus(self, 'EventsEventBus00default00hob1r',
-    #       tags = [
-    #       ],
-    #       name = 'default',
-    #     )
-    # eventsEventBus00default00hob1r.cfn_options.deletion_policy = cdk.CfnDeletionPolicy.RETAIN
-
     sqsQueue00SnsSubscriptionSendFailure0087EhT = sqs.CfnQueue(self, 'SQSQueue00SNSSubscriptionSendFailure0087EhT',
           sqs_managed_sse_enabled = True,
           receive_message_wait_time_seconds = 0,
           delay_seconds = 0,
           message_retention_period = 345600,
           maximum_message_size = 262144,
-          visibility_timeout = 30,
+          visibility_timeout = 60,
           queue_name = 'SNSSubscriptionSendFailure',
           tags = [
             {
@@ -52,26 +44,4 @@ class MySnsTopicWithSqsdlqStack(Stack):
           topic_name = 'kfsolutions-email',
         )
     snsTopic00kfsolutionsemail003vA2k.cfn_options.deletion_policy = cdk.CfnDeletionPolicy.RETAIN
-
-    # eventsRule00ruletrustedadvisro00zBCzW = events.CfnRule(self, 'EventsRule00ruletrustedadvisro00zBCzW',
-    #       event_bus_name = eventsEventBus00default00hob1r.ref,
-    #       event_pattern = {
-    #         'detail-type': [
-    #           'Trusted Advisor Check Item Refresh Notification',
-    #         ],
-    #         'source': [
-    #           'aws.trustedadvisor',
-    #         ],
-    #       },
-    #       targets = [
-    #         {
-    #           'arn': snsTopic00kfsolutionsemail003vA2k.ref,
-    #           'id': 'trustedadvisro',
-    #         },
-    #       ],
-    #       state = 'ENABLED',
-    #       name = 'trustedadvisro',
-    #     )
-    # eventsRule00ruletrustedadvisro00zBCzW.cfn_options.deletion_policy = cdk.CfnDeletionPolicy.RETAIN
-
 
